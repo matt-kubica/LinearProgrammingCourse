dbbf2f1c50ddd248ce5bc112276bb3dd959ff6e8
qbic22562 <mateusz.kubica99@gmail.com>
2020-11-02 20:48:00 +0100

    Added examples

--- lab4/example_04_solvable_artificial_vars.py
+++ lab4/example_04_solvable_artificial_vars.py
@@ -2,12 +2,28 @@ import logging
 from saport.simplex.model import Model 
 
 def run():
-    #TODO:
-    # fill missing test based on the example_01_solvable.py
-    # to make the test a bit more interesting:
-    # * make the solver use artificial variables!
-    logging.info("This test is empty but it shouldn't be, fix it!")
-    raise AssertionError("Test is empty")
+    model = Model("example_04_artificial_variables")
+
+    x1 = model.create_variable("x1")
+    x2 = model.create_variable("x2")
+
+    model.add_constraint(2 * x1 - x2 <= -1)
+    model.add_constraint(1 * x1 + x2 == 3)
+
+    model.maximize(1 * x1 + 3 * x2)
+
+    try:
+        solution = model.solve()
+    except:
+        raise AssertionError(
+            "This problem has a solution and your algorithm hasn't found it!")
+
+    logging.info(solution)
+
+    assert (solution.assignment == [0.0, 3.0]
+            ), "Your algorithm found an incorrect solution!"
+
+    logging.info("Congratulations! This solution seems to be alright :)")
 
 
 if __name__ == '__main__':
--- lab4/example_05_unfeasible.py
+++ lab4/example_05_unfeasible.py
@@ -1,14 +1,25 @@
+
 import logging
-from saport.simplex.model import Model 
+from saport.simplex.model import Model
+
 
 def run():
-    #TODO:
-    # fill missing test based on the example_01_solvable.py
-    # to make the test a bit more interesting:
-    # * make the model unfeasible in a way detectable by the 2-phase simplex
-    # 
-    logging.info("This test is empty but it shouldn't be, fix it!")
-    raise AssertionError("Test is empty")
+    model = Model("example_05_unfeasible")
+
+    x1 = model.create_variable("x1")
+    x2 = model.create_variable("x2")
+    x3 = model.create_variable("x3")
+
+    model.maximize(x1 + x2)
+    model.add_constraint(x1 + x2 + x3 == 10)
+    model.add_constraint(x1 - x2 + x3 >= 100)
+
+    try:
+        solution = model.solve()
+    except:
+        logging.info("Congratulations! You found an unfesiable solution detectable with artificial variables :)")
+    else:
+        raise AssertionError("This problem has no solution but your algorithm hasn't figured it out!")
 
 
 if __name__ == '__main__':
