5eaefb5e425ff7690b4264dfc487a708da5cc0ce
qbic22562 <mateusz.kubica99@gmail.com>
2020-11-10 01:28:25 +0100

    Exercises

--- lab5/zad1.py
+++ lab5/zad1.py
@@ -1,2 +1,32 @@
 # remember to print the dual model (just print()) and the analysis results (analyser.interpret_results(solution, analysis_results))
-# in case of doubt refer to examples 06 and 07 
+# in case of doubt refer to examples 06 and 07
+
+from saport.simplex.analyser import Analyser
+from saport.simplex.model import Model
+
+
+def run():
+    primal = Model("zad1")
+
+    ss = primal.create_variable("SS")
+    s = primal.create_variable("S")
+    o = primal.create_variable("O")
+
+    primal.add_constraint(2 * ss + 2 * s + 5 * o <= 40)
+    primal.add_constraint(ss + 3 * s + 2 * o <= 30)
+    primal.add_constraint(3 * ss + s + 3 * o <= 30)
+    primal.maximize(32 * ss + 24 * s + 48 * o)
+
+    dual = primal.dual()
+
+    primal_solution = primal.solve()
+
+    print(dual)
+
+    analyser = Analyser()
+    analysis_results = analyser.analyse(primal_solution)
+    analyser.interpret_results(primal_solution, analysis_results)
+
+
+if __name__ == '__main__':
+    run()
--- lab5/zad2.py
+++ lab5/zad2.py
@@ -1,2 +1,31 @@
 # remember to print the dual model (just print()) and the analysis results (analyser.interpret_results(solution, analysis_results))
-# in case of doubt refer to examples 06 and 07 
+# in case of doubt refer to examples 06 and 07
+
+from saport.simplex.analyser import Analyser
+from saport.simplex.model import Model
+
+
+def run():
+    primal = Model("zad1")
+
+    l = primal.create_variable("l")
+    s = primal.create_variable("s")
+    k = primal.create_variable("k")
+
+    primal.add_constraint(8 * l + 6 * s + k <= 960)
+    primal.add_constraint(8 * l + 4 * s + 3 * k <= 800)
+    primal.add_constraint(4 * l + 3 * s + k <= 320)
+    primal.maximize(60 * l + 30 * s + 20 * k)
+
+    dual = primal.dual()
+    primal_solution = primal.solve()
+
+    print(dual)
+
+    analyser = Analyser()
+    analysis_results = analyser.analyse(primal_solution)
+    analyser.interpret_results(primal_solution, analysis_results)
+
+
+if __name__ == '__main__':
+    run()
